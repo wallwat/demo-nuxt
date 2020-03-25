03ea7f223fbe65646eee4c75ba1f0f9fe54efb91
chaiwat.r <chaiwat.r@linecorp.com>
2020-03-25 16:48:41 +0700

    initial project

---
+++ layouts/default.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'default page'
+    }
+  },
+  mounted () {
+    this.$nextTick(async () => {
+      this.liff = window.liff
+      const liffId = '1553887502-4Dop0qr8'
+      const path = this.$route.path
+      if (path && path !== '/') {
+        localStorage.setItem('redirectPath', String(path))
+      }
+      await this.liff.init({ liffId }, async () => {
+        try {
+          console.log('isLoggedIn', this.liff.isLoggedIn())
+          console.log(this.$route)
+
+          if (!this.liff.isLoggedIn()) {
+            this.liff.login({ redirectUri: `http://localhost.com:3000` })
+          } else {
+            const profile = await this.liff.getProfile()
+            this.$store.commit('updateProfile', profile)
+
+            const redirectPath = localStorage.getItem('redirectPath')
+            if (redirectPath) {
+              this.$router.push({ path: redirectPath })
+              localStorage.removeItem('redirectPath')
+            }
+          }
+        } catch (e) {
+          console.log(e)
+        }
+      })
+    })
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,57 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [{
+      src: 'https://line-objects-internal.com/assets/liff/edge/2.1/sdk.js'
+    }, {
+      src: 'https://wechatfe.github.io/vconsole/lib/vconsole.min.js?v=3.2.0'
+    }]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
